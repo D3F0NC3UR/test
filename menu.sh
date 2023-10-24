1ff518d2c3957a3f2e496ed3577635203c328391
D3F0NC3UR <19541806+D3F0NC3UR@users.noreply.github.com>
2023-10-23 21:17:01 -0400

    Update menu.sh#!/bin/bash
    ##
    # BASH menu script that checks:
    #   - Memory usage
    #   - CPU load
    #   - Number of TCP connections 
    #   - Kernel version
    ##
    
    server_name=$(hostname)
    
    function memory_check() {
     echo ""
    	echo "Memory usage on ${server_name} is: "
    	free -h
    	echo ""
    }
    
    function cpu_check() {
        echo ""
    	echo "CPU load on ${server_name} is: "
        echo ""
    	uptime
        echo ""
    }
    
    function tcp_check() {
    echo ""
    echo "TCP connections on ${server_name}: "
    echo ""
    	cat  /proc/net/tcp | wc -l
        echo ""
    }
    
    function kernel_check() {
        echo ""
    	echo "Kernel version on ${server_name} is: "
    	echo ""
    	uname -r
        echo ""
    }
    
    function all_checks() {
    	memory_check
    	cpu_check
    	tcp_check
    	kernel_check
    }
    
    ##
    # Color  Variables
    ##
    green='\e[32m'
    blue='\e[34m'
    clear='\e[0m'
    
    ##
    # Color Functions
    ##
    
    ColorGreen(){
    	echo -ne $green$1$clear
    }
    ColorBlue(){
    	echo -ne $blue$1$clear
    }
    
    menu(){
    echo -ne "
    My First Menu
    $(ColorGreen '1)') Memory usage
    $(ColorGreen '2)') CPU load
    $(ColorGreen '3)') Number of TCP connections 
    $(ColorGreen '4)') Kernel version
    $(ColorGreen '5)') Check All
    $(ColorGreen '6)') package install
    $(ColorGreen '0)') Exit
    $(ColorBlue 'Choose an option:') "
            read a
            case $a in
    	        1) memory_check ; menu ;;
    	        2) cpu_check ; menu ;;
    	        3) tcp_check ; menu ;;
    	        4) kernel_check ; menu ;;
    	        5) all_checks ; menu ;;
            6) menu2 ;;
    		0) exit 0 ;;
    		*) echo -e $red"Wrong option."$clear; WrongCommand;;
            esac
    }
    
    # Call the menu function
    menu
    
    menu2(){
    echo -ne "
    My First Menu
    $(ColorGreen '1)') Memory usage
    $(ColorGreen '2)') CPU load
    $(ColorGreen '3)') Number of TCP connections 
    $(ColorGreen '4)') Kernel version
    $(ColorGreen '5)') Check All
    $(ColorGreen '6)') package install
    $(ColorGreen '0)') Exit
    $(ColorBlue 'Choose an option:') "
            read a
            case $a in
    	        1) memory_check ; menu ;;
    	        2) cpu_check ; menu ;;
    	        3) tcp_check ; menu ;;
    	        4) kernel_check ; menu ;;
    	        5) all_checks ; menu ;;
            6) menu2 ;;
    		0) exit 0 ;;
    		*) echo -e $red"Wrong option."$clear; WrongCommand;;
            esac
    }
    
    # Call the menu function
    menu2

--- menu.sh
+++ menu.sh
@@ -11,7 +11,7 @@
 server_name=$(hostname)
 
 function memory_check() {
-    echo ""
+ echo ""
 	echo "Memory usage on ${server_name} is: "
 	free -h
 	echo ""
@@ -26,9 +26,9 @@ function cpu_check() {
 }
 
 function tcp_check() {
-    echo ""
-	echo "TCP connections on ${server_name}: "
-    echo ""
+echo ""
+echo "TCP connections on ${server_name}: "
+echo ""
 	cat  /proc/net/tcp | wc -l
     echo ""
 }
@@ -74,6 +74,7 @@ $(ColorGreen '2)') CPU load
 $(ColorGreen '3)') Number of TCP connections 
 $(ColorGreen '4)') Kernel version
 $(ColorGreen '5)') Check All
+$(ColorGreen '6)') package install
 $(ColorGreen '0)') Exit
 $(ColorBlue 'Choose an option:') "
         read a
@@ -83,6 +84,7 @@ $(ColorBlue 'Choose an option:') "
 	        3) tcp_check ; menu ;;
 	        4) kernel_check ; menu ;;
 	        5) all_checks ; menu ;;
+        6) menu2 ;;
 		0) exit 0 ;;
 		*) echo -e $red"Wrong option."$clear; WrongCommand;;
         esac
@@ -90,3 +92,30 @@ $(ColorBlue 'Choose an option:') "
 
 # Call the menu function
 menu
+
+menu2(){
+echo -ne "
+My First Menu
+$(ColorGreen '1)') Memory usage
+$(ColorGreen '2)') CPU load
+$(ColorGreen '3)') Number of TCP connections 
+$(ColorGreen '4)') Kernel version
+$(ColorGreen '5)') Check All
+$(ColorGreen '6)') package install
+$(ColorGreen '0)') Exit
+$(ColorBlue 'Choose an option:') "
+        read a
+        case $a in
+	        1) memory_check ; menu ;;
+	        2) cpu_check ; menu ;;
+	        3) tcp_check ; menu ;;
+	        4) kernel_check ; menu ;;
+	        5) all_checks ; menu ;;
+        6) menu2 ;;
+		0) exit 0 ;;
+		*) echo -e $red"Wrong option."$clear; WrongCommand;;
+        esac
+}
+
+# Call the menu function
+menu2
