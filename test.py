abac37bb4f0108bbb197978cf62062a9a9d63ebf
D3F0NC3UR <19541806+D3F0NC3UR@users.noreply.github.com>
2023-12-08 12:38:46 -0500

    update test.py

--- test.py
+++ test.py
@@ -1,76 +1,74 @@
 #pylint:disable=C0301
-
 import os
 
 def afficher_menu_principal():
-    print("1. Exécuter des scripts Bash")
-    print("2. cmd")
-    print("3. Quitter")
+print("1. Exécuter des scripts Bash")
+print("2. cmd")
+print("3. Quitter")
 
 def afficher_sous_menu_bash():
-    print("Sous-menu - Scripts Bash:")
-    print("1. Script 1")
-    print("2. Script 2")
-    print("3. Retour au menu principal")
+print("Sous-menu - Scripts Bash:")
+print("1. Script 1")
+print("2. Script 2")
+print("3. Retour au menu principal")
 
 def executer_script_bash(script):
-    os.system(script)
+os.system(script)
 
 def executer_commande_terminal(commande):
-    os.system(commande)
+os.system(commande)
 
 def afficher_sous_menu_commandes():
-    print("Sous-menu - Commandes prédéfinies:")
-    print("1. Commande 1")
-    print("2. Commande 2")
-    print("3. Retour au menu principal")
+print("Sous-menu - Commandes prédéfinies:")
+print("1. Commande 1")
+print("2. Commande 2")
+print("3. Retour au menu principal")
 
 def main():
-    while True:
-        afficher_menu_principal()
-        choix_principal = input("Sélectionnez une option : ")
+while True:
+afficher_menu_principal()
+choix_principal = input("Sélectionnez une option : ")
 
-        if choix_principal == "1":
-            while True:
-                afficher_sous_menu_bash()
-                choix_bash = input("Sélectionnez un script Bash : ")
+if choix_principal == "1":
+while True:
+afficher_sous_menu_bash()
+choix_bash = input("Sélectionnez un script Bash : ")
 
-                if choix_bash == "1":
-                    executer_script_bash("bash_script_1.sh")
+if choix_bash == "1":
+executer_script_bash("bash_script_1.sh")
 
-                elif choix_bash == "2":
-                    executer_script_bash("bash_script_2.sh")
+elif choix_bash == "2":
+executer_script_bash("bash_script_2.sh")
 
-                elif choix_bash == "3":
-                    break
+elif choix_bash == "3":
+break
 
-                else:
-                    print("Choix invalide. Veuillez sélectionner une option valide.")
+else :
+print("Choix invalide. Veuillez sélectionner une option valide.")
 
-        elif choix_principal == "2":
-            while True:
-                afficher_sous_menu_commandes()
-                choix_commande = input("Sélectionnez une commande prédéfinie : ")
+elif choix_principal == "2":
+while True:
+afficher_sous_menu_commandes()
+choix_commande = input("Sélectionnez une commande prédéfinie : ")
 
-                if choix_commande == "1":
-                	            executer_commande_terminal("sudo apt update && apt upgrade -y && apt install git")
-                
-                elif choix_commande == "2":
-                    executer_commande_terminal("commande_2")
+if choix_commande == "1":
+executer_commande_terminal("sudo apt update && apt upgrade -y && apt install git")
 
-                elif choix_commande == "3":
-                    break
+elif choix_commande == "2":
+executer_commande_terminal("commande_2")
 
-                else:
-                    print("Choix invalide. Veuillez sélectionner une option valide.")
+elif choix_commande == "3":
+break
 
-        elif choix_principal == "3":
-            print("Au revoir !")
-            break
+else :
+print("Choix invalide. Veuillez sélectionner une option valide.")
 
-        else:
-            print("Choix invalide. Veuillez sélectionner une option valide.")
+elif choix_principal == "3":
+print("Au revoir !")
+break
+
+else :
+print("Choix invalide. Veuillez sélectionner une option valide.")
 
 if __name__ == "__main__":
-    main()
-    
+main()
