65afb6ac91eb8ab462a8e815470d83d5c34185cd
D3F0NC3UR <19541806+D3F0NC3UR@users.noreply.github.com>
2023-12-11 23:15:40 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -72,7 +72,7 @@ curl -fsSL https://pkgs.tailscale.com/stable/debian/bookworm.noarmor.gpg | sudo 
 curl -fsSL https://pkgs.tailscale.com/stable/debian/bookworm.tailscale-keyring.list | sudo tee /etc/apt/sources.list.d/tailscale.list
 sudo apt update
 sudo apt install tailscale
-echo("succes")
+echo"succes"
     echo ""
 }
 
